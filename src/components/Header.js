84a40e2aedac14fb030d81bf50fcf69dce171238
Jason <jasonbekker17@gmail.com>
2024-07-13 10:42:06 +0300

    inital commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Sekiro from '../assets/pic1.jpg'
+
+
+const Header = () => {
+  return (
+    <div>
+        <img className='logo' alt="SekiroImage" src={Sekiro}></img>
+        <h1>Sekiro  Header above</h1>
+      
+    </div>
+  )
+}
+
+export default Header
