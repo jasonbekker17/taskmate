3e230a92201ee3d344a005659f3f18f8ca8f904e
Jason <jasonbekker17@gmail.com>
2024-07-15 10:22:58 +0300

    added some  css and toggling show task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { useState } from 'react'
+
 import './App.css'
 
 
 const App = () => {
+const [tasks ,setTasks]=useState([{id:1,name:"Clean Room",completed:true},{id:2,name:"Study React",completed:false},{id:3,name:"Light Up",completed:false}])
+const[show,setShow]=useState(true)
+
+
+function handleDelete(id){
+  setTasks(tasks.filter(task=>id!==task.id))
+
+}
+
+
+
   return (
-    <div>
-      <Header/>
-      <div>
-        <h1> this is the body</h1>
-      </div>
-      <Footer/>
+    <div className='App'>
+      
+      <h1>Task List</h1>
+      <ul>
+        <button className='trigger' onClick={()=>{setShow(!show)}}>Show Task</button>
+        {show &&tasks.map((task) =>(  
+                 
+                 <li className={task.completed ? "completed":"incomplete"} key={task.id}>{task.id}- {task.name}
+                 <button className='delete' onClick={()=>handleDelete(task.id)}>Delete</button>
+                 </li>
+               ))}
+      </ul>
     </div>
   )
 }
